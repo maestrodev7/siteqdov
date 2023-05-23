56adc3b0bc26b5ad015e4737a3a1cca03c550906
julien <youmbidefonam2@gmail.com>
2023-05-23 12:50:15 +0200

    anothe test

--- src/components/SignupUser.vue
+++ src/components/SignupUser.vue
@@ -490,13 +490,27 @@ export default {
     async goregister(){
             const provider = new GoogleAuthProvider();
             const auth = getAuth();
-            try {
-
-            const result = await signInWithPopup(auth, provider)
-              console.log(result.user)
-            } catch(error) {
-                console.log("error : ", error);
-            }
+            signInWithPopup(auth, provider)
+            .then((result) => {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              const credential = GoogleAuthProvider.credentialFromResult(result);
+              const token = credential.accessToken;
+              console.log(" token " + token);
+              // The signed-in user info.
+              const user = result.user;
+              console.log("info user " + user);
+              // IdP data available using getAdditionalUserInfo(result)
+              // ...
+            }).catch((error) => {
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              // The email of the user's account used.
+              const email = error.customData.email;
+              // The AuthCredential type that was used.
+              const credential = GoogleAuthProvider.credentialFromError(error);
+              // ...
+            });
           },
     validate() {
       if (this.$refs.loginForm.validate()) {
